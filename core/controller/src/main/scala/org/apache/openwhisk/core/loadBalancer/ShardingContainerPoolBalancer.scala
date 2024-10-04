f7b44004d49c57431b7db65f101e662f82652081
Paola <paolacharabaty@gmail.com>
2024-10-04 11:03:21 -0400

    debugging

--- core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/ShardingContainerPoolBalancer.scala
+++ core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/ShardingContainerPoolBalancer.scala
@@ -404,7 +404,6 @@ object ShardingContainerPoolBalancer extends LoadBalancerProvider {
     stepsDone: Int = 0)(implicit logging: Logging, transId: TransactionId): Option[(InvokerInstanceId, Boolean)] = {
     val numInvokers = invokers.size
     logging.info(this, "AAAAAAAAAAHHHOMMMMMMMMMMAAZZZBBBBBBBBBBBBBBB")
-    emitMetrics()
     if (numInvokers > 0) {
       val invoker = invokers(index)
       //test this invoker - if this action supports concurrency, use the scheduleConcurrent function
