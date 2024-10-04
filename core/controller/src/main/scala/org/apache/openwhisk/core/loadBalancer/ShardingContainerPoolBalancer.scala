a06e112b6e80618e60ea1817a93fd3d936b64bd9
Paola <paolacharabaty@gmail.com>
2024-10-04 10:21:16 -0400

    more logs

--- core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/ShardingContainerPoolBalancer.scala
+++ core/controller/src/main/scala/org/apache/openwhisk/core/loadBalancer/ShardingContainerPoolBalancer.scala
@@ -406,6 +406,7 @@ object ShardingContainerPoolBalancer extends LoadBalancerProvider {
     if (numInvokers > 0) {
       val invoker = invokers(index)
       //test this invoker - if this action supports concurrency, use the scheduleConcurrent function
+      emitMetrics()
       logging.info(this, s"Total user memory for blackbox invokers: $curr.id.userMemory.toMB MB")
       if (invoker.status.isUsable && dispatched(invoker.id.toInt).tryAcquireConcurrent(fqn, maxConcurrent, slots)) {
         Some(invoker.id, false)
